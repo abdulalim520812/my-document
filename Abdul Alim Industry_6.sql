c0c6c70080095b2874eb3dec15a6603b800e06d5
Md. Abdul Alim <alim520812@gmail.com>
2025-02-08 20:51:52 +0600

    Add files via upload

--- Abdul Alim Industry_6.sql
+++ Abdul Alim Industry_6.sql
@@ -125,6 +125,8 @@ COMMIT;
 END IF;
 
 END $$
+
+
 DELIMITER ;
 
 
@@ -142,14 +144,73 @@ CREATE TABLE products(
 
 
 
+
+
 SELECT products.name AS product_name, shops.name AS shop_name, users.name AS user_name
 FROM products JOIN shops ON products.shop_id = shops.id
 JOIN users ON shops.user_id = users.id;
+
+
+
 
 
 CREATE VIEW product_shop_user_view AS
 SELECT products.name AS product_name, shops.name AS shop_name, users.name AS owner_name
 FROM products JOIN shops ON products.shop_id = shops.id
 JOIN users ON shops.user_id = users.id;
+
+
+
+
+
+
+
+DELIMITER $$ 
+
+CREATE PROCEDURE  addNewProduct(
+IN p_name VARCHAR(100),
+IN p_price DECIMAL(10,2),
+IN p_stock int,
+IN p_category_id int,
+IN p_shop_id int
+)
+BEGIN 
+INSERT INTO products(name, price, stock, category_id, shop_id)
+VALUES(p_name, p_price, p_stock, p_category_id, p_shop_id);
+END$$
+DELIMITER ;
+
+
+
+
+
+CALL addNewShop('Oishe Super Shop', 1);
+
+CALL addNewProduct('potato', 70, 100, 2, 2);
+
+
+DELIMITER $$
+CREATE FUNCTION calculatePrice(p_name varchar(100), p_quantity int)
+RETURNS varchar(100)
+BEGIN
+DECLARE p_price decimal(10,2);
+DECLARE p_stock int;
+SELECT price, stock INTO p_price, p_stock FROM products WHERE name = p_name;
+IF p_stock >= p_quantity THEN
+RETURN CONCAT('Total Price: ', p_price * p_quantity) ;
+
+
+ELSE RETURN 'Product Not Available';
+END IF;
+
+END $$
+DELIMITER ;
+
+
+
+FIND POTATO PRICE?
+SELECT calculatePrice('potato', 20);
+
+
 
 
