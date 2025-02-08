4e5d375d03388368dd65928a1871f8d8febd5986
Md. Abdul Alim <alim520812@gmail.com>
2025-02-08 20:58:17 +0600

    Add files via upload

---
+++ Alim SES 06.sql
@@ -0,0 +1,56 @@
+
+Industry class_5
+
+
+DElimiter $$
+CREATE PROCEDURE getDeptName(IN dept_id int, OUT dept_name VARCHAR(100))
+BEGIN
+SELECT title FROM departments WHERE id = dept_id;
+end $$
+DElimiter ;
+
+
+call getDeptName(1, @name)
+
+SELECT @name;
+
+call addLog(1, @name)
+
+SELECT COUNT(title) FROM departments WHERE id = dept_id;
+
+IF ... THEN
+....
+ELSE
+......
+END IF;
+
+
+BEGIN 
+START TRANSACTION;
+INSERT INTO ...;
+ INSERT INTO ...;
+INSERT INTO ...;
+COMMIT
+END$$
+
+
+BEGIN 
+START TRANSACTION;
+INSERT INTO ...;
+ INSERT INTO ...;
+INSERT INTO ...;
+ROLLBACK; 
+
+END$$
+
+DELIMITER $$
+
+CREATE FUNCTION getTotalDept()
+RETURNS INT;
+BEGIN
+	DECLARE total int;
+	SELECT COUNT(*) INTO total FROM departments;
+    RETURN total;
+END $$
+
+DELIMITER ;
