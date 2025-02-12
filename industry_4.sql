4bc5f04f4fdb80a727e7a2febaa3a26b44e332c7
Md. Abdul Alim <alim520812@gmail.com>
2025-02-12 18:59:37 +0600

    Add files via upload

---
+++ industry_4.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 12, 2025 at 01:58 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `industry_4`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addDepartment` (IN `dept_name` VARCHAR(100))   BEGIN
+	INSERT INTO department(name) VALUES(dept_name);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addDepartmentInfo` (IN `dept_name` VARCHAR(100), `dept_contact` VARCHAR(50))   BEGIN 
+
+INSERT INTO department(name, contact)
+VALUES (dept_name,dept_contact);
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getDeptInfo` (IN `dept_id` INT, OUT `dept_name` VARCHAR(100))   BEGIN 
+
+SELECT name FROM department WHERE (id= dept_id);
+
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getValues` ()   BEGIN 
+
+SELECT * FROM department;
+end$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `contact` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `name`, `contact`) VALUES
+(1, 'SES', '23456789876'),
+(2, 'CSE', '4567890987'),
+(3, 'CDM', '56765433345'),
+(4, 'Bangla', '4567890987'),
+(5, 'English', ''),
+(6, 'Mathematics', '456787654');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `contact` varchar(50) DEFAULT NULL,
+  `dept_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `contact`, `dept_id`) VALUES
+(1, 'Alim', '01837763581', 1),
+(2, 'Fahim', '4567890987', 3),
+(3, 'Mehetab', '2345678987', 4),
+(4, 'Musfik', '45678909', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dept_id` (`dept_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`dept_id`) REFERENCES `department` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
